be0bc966b7bcbf73d6aa19b4bb8a62007ca8e400
Danilo Vucetic <danilo.vucetic@mila.quebec>
2024-03-12 15:27:25 -0400

    Add env for RPS and wandb
    Danilo

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,9 @@
 """
 Proof of concept for rock-paper-scissors toy game.
+
+TODO: Add target-based surrogate optimisation and methods to differentiate between them.
+TODO: Add hydra for configuration selection and easy switching between methods.
+TODO: Add random seed setting to get repeatable runs!
 """
 
 
@@ -7,8 +11,9 @@ import torch
 from torch.nn import Module
 from torch.nn import Parameter
 from functorch import make_functional, jacrev, vmap
-from rock_paper_scissors_env import RPSEnv # Won't use, but it is easy to use.
+from rock_paper_scissors_env import RPSEnv
 from tqdm import tqdm
+import wandb
 
 
 class TabularRPSPlayer(Module):
@@ -38,26 +43,6 @@ class MLPRPSPlayer(Module):
 
     def forward(self, x):
         return self.network(x)
-
-
-def RPS_objective(pred_1, pred_2, reward_mat, current_player, lambda_reg=0.1):
-    multiplier = 1
-    # breakpoint()
-    if(current_player == 1):
-        # Player 1 is being learned, so update player 1, and freeze player 2
-        pred_2 = pred_2.detach()
-    else:
-        # Freeze p1, learn p2
-        pred_1 = pred_1.detach()
-        multiplier = -1
-    
-    # Now, we have the policies of each player, compute the loss
-    reward = torch.sum(pred_1 @ reward_mat @ pred_2.T)
-    loss = reward + (lambda_reg / 2) * (
-        torch.linalg.vector_norm(pred_1 - 1/3)**2 + 
-        torch.linalg.vector_norm(pred_2 - 1/3)**2
-    )
-    return multiplier * loss
 
 def compute_jacobian_wrt_params(player, input):
     """
@@ -98,8 +83,12 @@ def compute_jacobian_wrt_params(player, input):
     )
     return natural_grad_outer_prod_inv
 
+# TODO: implement custom optimiser that can handle this for us. Extremely ugly and annoying code!
 @torch.no_grad()
-def compute_update(player, inv_outer_prod_jac, lr):
+def compute_NG_update(player, inv_outer_prod_jac, lr):
+    """
+    Natural gradient parameter update.
+    """
     # Get all the gradients and concatenate them into a single tensor
     all_grads = torch.cat([
         param.grad.flatten() for param in player.parameters()
@@ -113,23 +102,24 @@ def compute_update(player, inv_outer_prod_jac, lr):
         param -= lr * param_grad
         idx += param_length
 
-def train(num_steps=1000, base_lr=1e-5):
+def train(num_steps=1000, base_lr=1e-6):
     """
     Train the simple rock-paper-scissors agents.
     """
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    reward_mat = torch.tensor([
-        [0, -1, 1],
-        [1, 0, -1],
-        [-1, 1, 0]],
-        device=device, dtype=torch.float)
+
+    env = RPSEnv(lambda_reg=1.0, device=device)
+    
     player_1 = MLPRPSPlayer(input_size=3, hidden_size=128, output_size=3).to(device)
     player_2 = MLPRPSPlayer(input_size=3, hidden_size=128, output_size=3).to(device)
+
     p1_opt = torch.optim.SGD(player_1.parameters(), lr=base_lr)
     p2_opt = torch.optim.SGD(player_2.parameters(), lr=base_lr)
+    
     player_1.train()
     player_2.train()
-    input = torch.tensor([[0, 1.0, 0]], device=device)
+    
+    input = torch.tensor([[0.0, 1.0, 0.0]], device=device)
     
     for step in (pbar := tqdm(range(num_steps), unit="Step")):
         # Schedule a linearly decreasing learning rate
@@ -140,48 +130,42 @@ def train(num_steps=1000, base_lr=1e-5):
         p2_pred = player_2(input)
 
         # Compute the loss for each player, and update their parameters:
-        p1_loss = RPS_objective(p1_pred, p2_pred, reward_mat, current_player=1, lambda_reg=1.0)
-        p2_loss = RPS_objective(p1_pred, p2_pred, reward_mat, current_player=2, lambda_reg=1.0)
+        p1_loss, p2_loss = env.hcc_objective(
+            p1_policy=p1_pred, p2_policy=p2_pred)
 
-        # Fnet, params_F = make_functional(player_1)
-        # Gnet, params_G = make_functional(player_2)
-        # inv_outer_prod_jac_1 = jac_outer_inv(Fnet, params_F, input, device)
-        # inv_outer_prod_jac_2 = jac_outer_inv(Gnet, params_G, input, device)
         inv_outer_prod_jac_1 = compute_jacobian_wrt_params(player_1, input)
         inv_outer_prod_jac_2 = compute_jacobian_wrt_params(player_2, input)
-        # breakpoint()
 
         # Now, compute the updates for player 1
         p1_opt.zero_grad()
         p1_loss.backward()
-        compute_update(player_1, inv_outer_prod_jac_1, lr)
-        # TODO: implement custom optimiser that can handle this for us. Extremely ugly and annoying code!
-        # player_1.super_complex_neural_network.data -= lr * torch.matmul(
-        #     inv_outer_prod_jac_1,
-        #     player_1.super_complex_neural_network.grad,
-        # )
+        compute_NG_update(player_1, inv_outer_prod_jac_1, lr)
 
         # Now, compute the updates for player 2
         p2_opt.zero_grad()
         p2_loss.backward()
-        # player_2.super_complex_neural_network.data -= lr * torch.matmul(
-        #     inv_outer_prod_jac_2,
-        #     player_2.super_complex_neural_network.grad,
-        # )
-        compute_update(player_2, inv_outer_prod_jac_2, lr)
-        # breakpoint()
-        # p2_opt.param_groups[0]
+        compute_NG_update(player_2, inv_outer_prod_jac_2, lr)
         
         if(step % 10 == 0):
-            # breakpoint()
             tqdm.write(f"loss = {p1_loss.item()}")
             tqdm.write(f"p1 policy = {p1_pred[0,0].item() :.2f}, {p1_pred[0,1].item() :.2f}, {p1_pred[0,2].item() :.2f}")
             tqdm.write(f"p2 policy = {p2_pred[0,0].item() :.2f}, {p2_pred[0,1].item() :.2f}, {p2_pred[0,2].item() :.2f}")
+        
+        wandb.log({
+            "loss": p1_loss.item(),
+            # "": ,
+            # "": ,
+        })
     
     return p1_pred, p2_pred, p1_loss.item()
 
 
 if __name__ == "__main__":
+    wandb.init(
+        project="HCC_NG_vs_TBS",
+        tags=["NG_vs_TBS"],
+        mode="online",
+    )
     p1, p2, loss = train()
 
 
--- rock_paper_scissors_env.py
+++ rock_paper_scissors_env.py
@@ -2,44 +2,48 @@ import torch
 
 class RPSEnv():
     """
-    Rock-paper-scissors environment for normal RL-style training.
+    Toy rock-paper-scissors environment in a hidden convex-concave game style.
     """
-    ROCK = 0
-    PAPER = 1
-    SCISSORS = 2
-    ACT_TO_TEXT = {
-        ROCK: "rock",
-        PAPER: "paper",
-        SCISSORS: "scissors",
-    }
-    TIE = 0
-    WIN = 1
-    LOSS = -1
-
-
-    def __init__(self, device):
+    def __init__(self, lambda_reg: float, device: torch.DeviceObjType):
         """
         Constructor for Rock-Paper-Scissors environment.
         """
+        self.lambda_reg = lambda_reg
         self.device = device
+        self.reward_mat = torch.tensor([
+            [0, -1, 1],
+            [1, 0, -1],
+            [-1, 1, 0]],
+            device=device, dtype=torch.float)
     
-    def step(
-            self,
-            p1_moves: torch.Tensor,
-            p2_moves: torch.Tensor,
-        ) -> torch.Tensor:
+    def __hcc_loss(self, p1_policy, p2_policy):
+        # We have the policies of each player, compute the loss
+        reward = torch.sum(p1_policy @ self.reward_mat @ p2_policy.T)
+        loss = reward + (self.lambda_reg / 2) * (
+            torch.linalg.vector_norm(p1_policy - 1/3)**2 + 
+            torch.linalg.vector_norm(p2_policy - 1/3)**2
+        )
+        return loss
+    
+    def hcc_objective(
+        self,
+        p1_policy: torch.Tensor,
+        p2_policy: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """
-        Take a step (i.e., play a batch of games), and return the results.
-
-        Returns the rewards from p1's perspective.
+        Compute the hidden convex-concave game objective given the player
+        policies. This method returns two losses:
+            1) The loss for the first player with player 2's gradient's
+                detached.
+            2) The loss for the second player with player 1's gradient's
+                detached.
         """
-        # Find when each player wins and when they draw, then assign rewards.
-        move_diff = p2_moves - p1_moves
-
-        p1_win = (move_diff == -1).long() + (move_diff == 2).long()
-        p2_win = (move_diff == 1).long() + (move_diff == -2).long()
-
-        rewards = torch.zeros_like(p1_win, device=self.device)
-        rewards += p1_win * self.WIN + p2_win * self.LOSS
-
-        return rewards
+        # Player 1 is being learned, so update player 1, and freeze player 2.
+        loss_p1 = self.__hcc_loss(p1_policy, p2_policy.detach())
+        
+        # Freeze p1, learn p2. Loss is negated since p2 is maximising.
+        loss_p2 = -self.__hcc_loss(p1_policy.detach(), p2_policy)
+        return loss_p1, loss_p2
+        
+        
+        
