Bug fix: causal entropy coefficient
The causal entropy coefficient for the GAIL baseline should have been positive, not negative!!
This change gives the baseline GAIL agent god-like performance. It's crazy. I guess this means
that the mode doesn't collapse since the policy is encouraged to be more uniform. Cool stuff!

Danilo

src/opt_algos/gail_updates.py
@@ -382,9 +382,8 @@ def gail_policy_update(
     # NOTE: doing this after the line search means we are updating the
     # params according to the causal entropy after we updated them for the
     # line search... This may not be ideal.
-    # TODO: Check the sign of causal entropy, it may not be correct!
     causal_entropy = cfg.training_hyperparams.ce_lambda *  torch.mean(
-        (-1) * agent_gammas *\
+        agent_gammas * \
         agent_model(agent_obs).log_prob(agent_actions)
     )
     trpo_loss += causal_entropy.detach()